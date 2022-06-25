3f453da0e4a51583d4711ea7178d935a0aa79018
Neo <neobabylone@gmail.com>
2022-06-25 14:34:27 +0200

    VideoService getVideoById(Neo)

--- Mydeos-core/src/main/java/fr/mydeos/core/service/VideoServiceInterface.java
+++ Mydeos-core/src/main/java/fr/mydeos/core/service/VideoServiceInterface.java
@@ -2,8 +2,13 @@ package fr.mydeos.core.service;
 
 import fr.mydeos.core.entity.Video;
 
+import java.util.List;
+
 public interface VideoServiceInterface {
 
     public void addVideo(Video video);
+
+    public Video getVideoById(String id);
+
 
 }
