9bcc4c65d1ce7b27c408b6af0ca29304619e86e7
mat <game@le-scarabit.xyz>
2022-06-25 14:17:43 +0200

    annotated

--- Mydeos-core/src/main/java/fr/mydeos/core/repository/VideoRepository.java
+++ Mydeos-core/src/main/java/fr/mydeos/core/repository/VideoRepository.java
@@ -1,9 +1,12 @@
 package fr.mydeos.core.repository;
 
 import fr.mydeos.core.entity.Video;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
+@Repository
 public class VideoRepository implements VideoRepositoryInterface {
     ArrayList<Video> videos = new ArrayList<>();
 
--- Mydeos-web/src/main/java/fr/mydeos/web/controller/VideoController.java
+++ Mydeos-web/src/main/java/fr/mydeos/web/controller/VideoController.java
@@ -1,4 +1,5 @@
 package fr.mydeos.web.controller;
 
+
 public class VideoController {
 }
